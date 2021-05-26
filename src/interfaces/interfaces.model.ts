38b60ef9615d749bfc3a0c796cbc245448804266
curtisblanchette <curtis@4iiz.com>
2021-05-25 22:12:53 -0700

    feat(master): update rating config interface

--- src/interfaces/interfaces.model.ts
+++ src/interfaces/interfaces.model.ts
@@ -21,12 +21,16 @@ interface ICacheConfig {
 }
 
 interface IRatingConfig {
-	1: string;
-	2: string;
-	3: string;
-	4: string;
-	5: string;
-	6: string;
+	callFlow: {
+		feedback: {
+			1: string;
+			2: string;
+			3: string;
+			4: string;
+			5: string;
+			6: string;
+		}
+	}
 }
 
 export interface IModelConfig {
