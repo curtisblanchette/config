4da808aa8ccf0c5f2935630dcb018bb78be01302
curtisblanchette <hello@curtisblanchette.com>
2022-05-11 11:46:53 -0700

    feat(master): remove @4iiz/logging dependency

--- src/index.ts
+++ src/index.ts
@@ -12,29 +12,26 @@ export * from './interfaces/interfaces.config';
 let interval;
 let storedConfig:IConfig;
 
-import { createLogger } from '@4iiz/logging';
-const logger = createLogger("P] Config");
-
 export async function read(): Promise<IConfig> {
 
 	// retrieves the stack specific configuration
-	logger.info(`Retrieving configuration from S3`);
+	console.info(`Retrieving configuration from S3`);
 	const config = await getS3Config() as IConfig;
 
 	// get the secret keys
-	logger.info(`Retrieving keys from Secrets Manager`);
+	console.info(`Retrieving keys from Secrets Manager`);
 	const secrets = await getSecret('secrets');
 
 	// merge the secrets into the config
-	logger.info(`Constructing configuration`);
+	console.info(`Constructing configuration`);
 	Object.keys(secrets).forEach(path=>_.set( config, path, secrets[path] ));
 
 	return config;
 }
 
 export async function refresh(every=86400): Promise<IConfig> {
-	
-	logger.info(`Setting refresh interval to ${every}`);
+
+	console.info(`Setting refresh interval to ${every}`);
 	interval = setInterval(async () => {
 		storedConfig = await read();
 	}, every);
@@ -51,14 +48,14 @@ export async function get(remote = false): Promise<IConfig> {
 		// read the local config in and parse it
 		const file = fs.readFileSync(path, 'utf-8');
 		storedConfig = YAML.parse( file );
-		logger.info(`Configuration Loaded from local/config.yml`);
+		console.info(`Configuration Loaded from local/config.yml`);
 
 	} else {
 
-		logger.info(`Reading configuration`);
+		console.info(`Reading configuration`);
 		if (remote || !storedConfig) {
 			storedConfig = await read();
-			logger.info(`Configuration Loaded from AWS`);
+			console.info(`Configuration Loaded from AWS`);
 		}
 
 	}
