b551669279f3153c29e36b78998fff34647da3c3
Curt Blanchette <6607883+curtisblanchette@users.noreply.github.com>
2022-12-27 12:48:55 -0800

    fix(master): add location config to interface barrel

--- src/interfaces/interfaces.config.ts
+++ src/interfaces/interfaces.config.ts
@@ -3,8 +3,10 @@ import { ISourcesConfig } from "./interfaces.sources";
 import { ICredentialsConfig } from "./interfaces.credentials";
 import { ISettingsConfig } from "./interfaces.settings";
 import { IVendorConfig } from './interfaces.vendors';
+import { ILocationConfig } from './interfaces.location';
 
 export interface IConfig {
+	location: ILocationConfig;
 	sources: ISourcesConfig;
 	credentials: ICredentialsConfig;
 	settings: ISettingsConfig;
